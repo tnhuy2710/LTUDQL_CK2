fb9605aa94fe25a8a6158f4efd55f47ab4be2ee4
tnhuy2710 <31790145+tnhuy2710@users.noreply.github.com>
2019-12-19 16:05:05 +0700

    Update delete_user.php

--- modules/user/delete_user.php
+++ modules/user/delete_user.php
@@ -4,51 +4,30 @@
     $connectionInfo = array( "Database"=>"Alibaba", "CharacterSet" => "UTF-8", "UID"=>"QuanLyTSCNTT", "PWD"=>"QuanLyTSCNTT");
 	$conn = sqlsrv_connect($serverName, $connectionInfo);
 		
-	if(!empty($_POST["uSER_INFO"]))
+	if(!empty($_POST["MA_ND"]))
 	{
-		$MA_ND = $_POST['uSER_INFO'][0];
-		$TEN_ND = $_POST['uSER_INFO'][1];		
-		$MA_DON_VI = $_POST['uSER_INFO'][2];
-		$EMAIL = $_POST['uSER_INFO'][3];
-
-		$sql = "SELECT *
-				FROM NGUOI_DUNG
-				WHERE MA_ND = '$MA_ND'";
-		$stmt = sqlsrv_query( $conn, $sql );
-		if( $stmt === false) {
+		$MA_ND = $_POST['MA_ND'];
+		$sql = "DELETE FROM PHAN_NHOM
+				WHERE MA_NGUOI_DUNG = '$MA_ND'";
+		$dELETE_NND = sqlsrv_query( $conn, $sql );
+		if( $dELETE_NND === false) {
 			die( print_r( sqlsrv_errors(), true) );
-		}		
-		if($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC))
-		{
-			echo "Đã có thông tin của người dùng này!";
-		}		
-		else
-		{
-			$sql = "INSERT INTO NGUOI_DUNG
-					VALUES ('$MA_ND', '$TEN_ND', '$MA_DON_VI', '$EMAIL')";
-			$iNSERT_ND = sqlsrv_query( $conn, $sql );
-			if( $iNSERT_ND === false) {
-				die( print_r( sqlsrv_errors(), true) );
-			}
-
-			if(!empty($_POST["new_GROUPS"]))
-			{
-				foreach($_POST["new_GROUPS"] as $item)
-				{
-					$sql = "INSERT INTO PHAN_NHOM
-							VALUES ('$MA_ND', '$item')";
-					$iNSERT_NND = sqlsrv_query( $conn, $sql );
-					if( $iNSERT_NND === false) {
-						die( print_r( sqlsrv_errors(), true) );
-					}
-				}
-			}
-
-			echo "Thêm thành công!";
-		}		
-	}
-	sqlsrv_free_stmt($stmt);
-	sqlsrv_free_stmt($iNSERT_ND);
-	sqlsrv_free_stmt($iNSERT_NND);
+			echo "Không thể xóa nhóm người dùng, xin vui lòng liên hệ hỗ trợ";
+			exit();
+		} 
+		
+      	$sql = "DELETE FROM NGUOI_DUNG
+		  		WHERE MA_ND = '$MA_ND'";      
+		$dELETE_ND = sqlsrv_query( $conn, $sql );
+		if( $dELETE_ND === false) {
+			die( print_r( sqlsrv_errors(), true) );
+			echo "Không thể xóa người dùng, xin vui lòng liên hệ hỗ trợ";
+			exit();
+		}
+				
+		echo "Xóa thành công!";				
+	}	
+	sqlsrv_free_stmt($dELETE_ND);
+	sqlsrv_free_stmt($dELETE_NND);
 	sqlsrv_close($conn);
 ?>
